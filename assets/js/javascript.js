bd3792c1e8f6c3e28f00448a6c6c6bb7437ca482
Oumezzine Farah <oumezzine.farah@yahoo.com>
2023-11-21 10:40:02 +0100

    add more js

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,4 +2,22 @@ function showalert() {
   alert("Welcome to Waste Wise Plastic");
 }
 const name = document.getElementById("nameId");
-function validationform() {}
+function ValidateEmail() {
+  var inputText = document.getElementById("emailId");
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  /*
+\w+ :the username
+([\.-]?\w+)* : (.) (-) 
+\w+ :  domain name
+(\.\w{2,3})+ : .com,.fr ...
+    */
+  if (inputText.value.match(mailformat)) {
+    alert("Valid email address!");
+    inputText.focus();
+    return true;
+  } else {
+    alert("You have entered an invalid email address!");
+    inputText.focus();
+    return false;
+  }
+}
